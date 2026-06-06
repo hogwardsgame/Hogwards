e46848f125f7caf2899493758cefb5051911ef11
hogwardsgame <hogwards.game.tg@gmail.com>
2026-06-06 20:24:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+import asyncio
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -11,15 +13,15 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
-def main():
+async def main():
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
     print("Bot started...")
 
-    app.run_polling()
+    await app.run_polling()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
