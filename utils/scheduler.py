8f8e2a059148d86c95dbc0d240edd4996787b883
hogwardsgame <hogwards.game.tg@gmail.com>
2026-06-07 00:01:02 +0300

    Add files via upload

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -6,22 +6,14 @@ logger = logging.getLogger(__name__)
 scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
 
 
-async def reset_house_cup():
-    """Reset house cup on the 1st of each month at 00:00 MSK."""
-    from database import get_pool
-    pool = await get_pool()
-    async with pool.acquire() as conn:
-        await conn.execute("UPDATE house_points SET points = 0")
+def reset_house_cup():
+    from database import reset_house_cup_points
+    reset_house_cup_points()
     logger.info("House Cup reset done.")
 
 
 def setup_scheduler():
-    # House cup reset: 1st of every month at 00:00 MSK
-    scheduler.add_job(
-        reset_house_cup,
-        CronTrigger(day=1, hour=0, minute=0),
-        id="house_cup_reset",
-        replace_existing=True,
-    )
+    scheduler.add_job(reset_house_cup, CronTrigger(day=1, hour=0, minute=0),
+                      id="house_cup_reset", replace_existing=True)
     scheduler.start()
     logger.info("Scheduler started.")
